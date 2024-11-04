406ab412d72fb72e0974ec5833fe4b49950a2490
حسن الخليف <roh.baba.11@gmail.com>
2024-11-04 20:46:08 +0300

    create auth firebase signup service

---
+++ lib/services/firebase/auth_firebase_service.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tarsheed/services/firebase/auth_firebase_service.dart';
+
+class FirebaseService {
+  Future<UserCredential> signup(username, emailAddress, password) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+      await credential.user?.updateDisplayName(username);
+      return credential;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw const WeakPasswordException(
+            message: 'The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        throw const EmailAlreadyInUseException(
+            message: 'The account already exists for that email.');
+      } else {
+        throw FirebaseAuthException;
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
