b91ba1de831efbd9b10277fd5d4ba6d999a20fc8
حسن الخليف <roh.baba.11@gmail.com>
2024-11-05 11:44:15 +0300

    implement logOut in home screen

--- lib/modules/home/home_screen.dart
+++ lib/modules/home/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tarsheed/core/constants/app_constants.dart';
+import 'package:tarsheed/core/constants/custom_exceptions.dart';
 import 'package:tarsheed/modules/home/views/home_view.dart';
+import 'package:tarsheed/modules/main/main_screen.dart';
+import 'package:tarsheed/services/firebase/auth_firebase_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -81,12 +84,24 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void logout(context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const HomeScreen(),
-      ),
-    );
+    AuthFirebaseService().signOut().then(
+      (_) {
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(
+              builder: (_) => const MainScreen(),
+            ),
+            (_) => false);
+      },
+    ).onError((error, stackTrace) {
+      if (error is PublicException) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(error.message)));
+      } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(error.toString())));
+      }
+    });
   }
 
   void profilePressed(context) {}
