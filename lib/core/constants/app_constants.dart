fee14f421c547a19f557579094602f5d52404dc4
حسن الخليف <roh.baba.11@gmail.com>
2024-11-26 15:48:02 +0300

    add cardShape to app constant

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -7,6 +7,10 @@ class AppConstants {
   // Color constants
   static const Color primaryColor = Color(0xFF007FFF);
   static const Color backgroundColor = Color(0x2E2E3EFF);
+  static const ShapeBorder cardShape = RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(Radius.circular(16)),
+    side: BorderSide(color: Colors.white),
+  );
 }
 
 enum Periodic { daily, monthly, yearly }
